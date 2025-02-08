88e4ad00faacab4dc3227d9daaedfd2afe592a31
Abdul Saboor <saboorjunaid1@gmail.com>
2025-02-08 11:05:01 +0500

    api error fix

--- Day 4/next.config.mjs
+++ Day 4/next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     images: {
         domains: ['cdn.sanity.io']
-    }
+    },
+    experimental: {
+        appDir: true,
+      },
+      reactStrictMode: true,
+      output: "standalone",
 };
 
 export default nextConfig;
