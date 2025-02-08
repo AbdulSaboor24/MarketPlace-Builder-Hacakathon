362bc70de1b5fb4b9f060cc1e55128a7ddb3acb9
Abdul Saboor <saboorjunaid1@gmail.com>
2025-02-08 12:12:55 +0500

    api url correction

--- Day 4/src/app/utils/api.ts
+++ Day 4/src/app/utils/api.ts
@@ -1,13 +1,13 @@
 import { Product } from "./types";
 
 export const fetchProducts = async (): Promise<Product[]> => {
-  const res = await fetch("/api/products");
+  const res = await fetch("https://market-place-builder-hacakathon.vercel.app/api/products");
   return res.json();
 };
 
 export const fetchProductById = async (id: string): Promise<Product | null> => {
   try {
-    const res = await fetch(`/api/products/${id}`);
+    const res = await fetch(`https://market-place-builder-hacakathon.vercel.app/api/products/${id}`);
     if (!res.ok) throw new Error("Failed to fetch product");
     
     const data = await res.json();
@@ -19,7 +19,7 @@ export const fetchProductById = async (id: string): Promise<Product | null> => {
 };
 
 export const fetchCategories = async (): Promise<string[]> => {
-  const res = await fetch("/api/category");
+  const res = await fetch("https://market-place-builder-hacakathon.vercel.app/api/category");
   const data: { category: string }[] = await res.json();
 
   return [...new Set(data.map((item: { category: string }) => item.category))];
